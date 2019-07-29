2fec1c35f6855e85f20820160f5137d70489ea73
newpiero <newpiero@yandex.ru>
2019-07-29 06:14:02 +0300

    task4

--- src/task_4/ChatServer.java
+++ src/task_4/ChatServer.java
@@ -1,4 +1,11 @@
 package task_4;
 
 public class ChatServer {
+    void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    void stop() {
+        System.out.println("Server stopped");
+    }
 }
